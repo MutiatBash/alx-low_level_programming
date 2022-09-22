93fbaa76d229c612e6ff4eda4f3a54ce9b519411
MutiatBash <mutiatbashua@gmail.com>
2022-09-22 14:51:44 -0700

    alphabet lower

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_alphabet - check description
+ * description: it prints alphabet in lower case followed by a new line
+ * Return: Nothing
+ */
+
+void print_alphabet(void)
+{
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
+		_putchar(i);
+	_putchar('\n');
+}
+
