a0cc28888a92b3ba71cc79057359a47d10dc85f2
MutiatBash <mutiatbashua@gmail.com>
2022-09-22 15:10:35 -0700

    slower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _islower - entry point
+ * description: checks for lowercase character
+ * @c: the integer value it receives
+ * Return: 1 if true, 0 if false
+ */
+
+int _islower(int c)
+{
+	int i = 'a';
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		/*refer int c*/
+		if (c == i)
+		{
+			return (1);
+		}
+	}
+	return (0);
+}
+
