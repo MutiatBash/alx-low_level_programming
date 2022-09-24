f958b734ce353a58d25eb8acd078db383b05733c
MutiatBash <mutiatbashua@gmail.com>
2022-09-23 21:40:11 -0700

    4 rev

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -6,6 +6,7 @@ char *_strncat(char *dest, char *src, int n);
 void print_number(int n);
 int _strcmp(char *s1, char *s2);
 char *_strncpy(char *dest, char *src, int n);
+void reverse_array(int *a, int n);
 
 #endif
 
