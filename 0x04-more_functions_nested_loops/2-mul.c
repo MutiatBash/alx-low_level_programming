33df127b4c3252f06999bc4de9b57c277f91afc6
MutiatBash <mutiatbashua@gmail.com>
2022-11-19 13:40:11 -0800

    more functions

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * mul - multiple imtegers
+ *
+ * @a: first input
+ * @b: second intput
+ *
+ * Return: return results
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
